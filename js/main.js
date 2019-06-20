4d8e0a41a8888a0e655ed82eb5489a9c6fafc59f
jenneferaguda <48757711+jenneferaguda@users.noreply.github.com>
2019-06-20 17:08:02 +0800

    fix: replace 'a' with button

--- js/main.js
+++ js/main.js
@@ -182,10 +182,12 @@ createRestaurantHTML = (restaurant) => {
     address.innerHTML = restaurant.address;
     li.append(address);
 
-    const more = document.createElement('a');
+    const more = document.createElement('button');
     more.innerHTML = 'View Details';
-    more.href = DBHelper.urlForRestaurant(restaurant);
-    li.append(more)
+    more.onclick = function() {
+        window.location.href = DBHelper.urlForRestaurant(restaurant);
+    };
+    li.append(more);
 
     return li
 }
